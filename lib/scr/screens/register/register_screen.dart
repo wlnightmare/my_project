ad0bc9788c766b7a91aac846026d41bc34168ee8
Farabi Askar <askar.farabi12@gmail.com>
2021-12-21 17:23:45 +0600

    Added containers and sizedboxes

--- lib/scr/screens/register/register_screen.dart
+++ lib/scr/screens/register/register_screen.dart
@@ -6,6 +6,7 @@ class RegisterScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
+      backgroundColor: Color(0xFFF3F4F6),
       navigationBar: CupertinoNavigationBar(
         backgroundColor: CupertinoColors.white,
         border: Border(),
@@ -16,12 +17,18 @@ class RegisterScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
+            SizedBox(height: 32),
             CupertinoTextField(
               placeholder: 'Логин',
               decoration: BoxDecoration(
                 color: CupertinoColors.white,
               ),
               padding: const EdgeInsets.symmetric(vertical: 19, horizontal: 16),
+            ),
+            Container(
+              height: 1,
+              color: Color(0xFFE0E6ED),
+              margin: const EdgeInsets.symmetric(horizontal: 16),
             ),
             CupertinoTextField(
               placeholder: 'Телефон',
@@ -30,6 +37,11 @@ class RegisterScreen extends StatelessWidget {
               ),
               padding: const EdgeInsets.symmetric(vertical: 19, horizontal: 16),
             ),
+            Container(
+              height: 1,
+              color: Color(0xFFE0E6ED),
+              margin: const EdgeInsets.symmetric(horizontal: 16),
+            ),
             CupertinoTextField(
               placeholder: 'Почта',
               decoration: BoxDecoration(
@@ -37,12 +49,32 @@ class RegisterScreen extends StatelessWidget {
               ),
               padding: const EdgeInsets.symmetric(vertical: 19, horizontal: 16),
             ),
+            Container(
+              height: 1,
+              color: Color(0xFFE0E6ED),
+              margin: const EdgeInsets.symmetric(horizontal: 16),
+            ),
             CupertinoTextField(
               placeholder: 'Пароль',
               decoration: BoxDecoration(
                 color: CupertinoColors.white,
               ),
               padding: const EdgeInsets.symmetric(vertical: 19, horizontal: 16),
+            ),
+            SizedBox(height: 327),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: CupertinoButton(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                color: Color(0xFF4631D2),
+                child: Text(
+                  'Создать аккаунт',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                onPressed: () {},
+              ),
             ),
           ],
         ),
