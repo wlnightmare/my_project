4c241e7ef95713f2fd563a29ef43cd74df370ad1
Farabi Askar <askar.farabi12@gmail.com>
2021-12-22 17:23:54 +0600

    routing const

---
+++ lib/scr/router/routing_const.dart
@@ -0,0 +1,12 @@
+const String MainRoute = '/';
+
+const String AuthRoute = '/auth/login';
+const String RegisterRoute = '/auth/register';
+
+const String HomeRoute = '/restaurants';
+const String MapRoute = '/restaurants/map';
+const String FavouritesRoute = '/restaurants/favourites';
+const String SearchRoute = '/restaurants/search';
+const String ProfileRoute = '/profile';
+
+const String RestaurantDetailRoute = '/restaurants/detail';
