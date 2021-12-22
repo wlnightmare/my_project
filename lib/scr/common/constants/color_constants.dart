cb7ab4c42456471a9841928f16fb79181b70afef
Farabi Askar <askar.farabi12@gmail.com>
2021-12-22 20:08:58 +0600

    Constant colors

---
+++ lib/scr/common/constants/color_constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/painting.dart';
+
+class AppColors {
+  static const Color scaffoldBackground = Color(0xFFF3F4F6);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color main = Color(0xFF4631D2);
+  static const Color divider = Color(0xFFE0E6ED);
+}
