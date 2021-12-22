9c1bc1bbb744f7eee69da4f0583654f9dbc3d02b
Farabi Askar <askar.farabi12@gmail.com>
2021-12-22 20:19:08 +0600

    Padding constants

---
+++ lib/scr/common/constants/padding_constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+
+class AppPaddings {
+  static const EdgeInsets horizontal = EdgeInsets.symmetric(horizontal: 16);
+  static const EdgeInsets vertical = EdgeInsets.symmetric(vertical: 20);
+}
