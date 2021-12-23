c87441fe7910952ec18ee8c711eb0cba68b3af84
Farabi Askar <askar.farabi12@gmail.com>
2021-12-23 14:31:33 +0600

    Adding tabs

--- lib/scr/screens/main/MainScreen.dart
+++ lib/scr/screens/main/MainScreen.dart
@@ -1,17 +1,39 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+import 'package:my_project/scr/screens/auth/auth_screen.dart';
+import 'package:my_project/scr/screens/register/register_screen.dart';
 
 class MainScreen extends StatelessWidget {
-  const MainScreen({Key? key, this.tabBar}) : super(key: key);
+  const MainScreen({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return CupertinoTabScaffold(
-        tabBar: CupertinoTabBar(items: [
-          BottomNavigationBarItem(icon: Icon(CupertinoIcons.home)),
-          BottomNavigationBarItem(icon: Icon(CupertinoIcons.gift)),
-          BottomNavigationBarItem(icon: Icon(CupertinoIcons.person)),
-        ]),
-        tabBuilder: (context));
+        tabBar: CupertinoTabBar(
+          items: [
+            BottomNavigationBarItem(icon: Icon(CupertinoIcons.home)),
+            BottomNavigationBarItem(icon: Icon(CupertinoIcons.gift)),
+            BottomNavigationBarItem(icon: Icon(CupertinoIcons.person)),
+          ],
+        ),
+        tabBuilder: (context, index) {
+          return CupertinoTabView(
+            builder: (context) {
+              switch (index) {
+                case 0:
+                  return AuthScreen();
+                case 1:
+                  return RegisterScreen();
+                case 2:
+                  return AuthScreen(); //Лента
+                case 3:
+                  return RegisterScreen(); //Карта
+                default:
+                  return RegisterScreen();
+              }
+            },
+          );
+        });
   }
 }
